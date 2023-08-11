186c6374f2863ef512afcdd8f8165d6491f52593
Mr Web3 <87449884+ysongit@users.noreply.github.com>
2023-08-10 21:17:01 -0400

    add nft.storage

--- src/components/musician/create-music-nft/AddCover.tsx
+++ src/components/musician/create-music-nft/AddCover.tsx
@@ -4,8 +4,10 @@ import Disc from "../../../public/assets/disc.png";
 import Vector from "../../../public/assets/Vector.png";
 import { IoIosArrowBack } from "react-icons/io";
 
-function AddCover({ setStep, step }: any) {
-  const saveFile = async () => {
+function AddCover({ setStep, step, setImage }: any) {
+  const saveFile = async (event) => {
+    const image = event.target.files[0];
+    setImage(image);
     setStep(3);
   };
   return (
@@ -28,23 +30,27 @@ function AddCover({ setStep, step }: any) {
           />
           <p
             className="absolute inset-0 m-auto mt-[250px] text-center font-semibold"
-            onClick={saveFile}
           >
             <span className="text-[#2237F1] text-xl font-medium underline">
               AddCover
             </span>{" "}
             album cover
           </p>
+          <input type='file' id='photo' onChange={saveFile} />
         </div>
       </div>
 
       <div className="flex items-center justify-center mt-12 ">
-        <button className="flex items-center px-4 py-2 border border-[#E1E1E1] rounded hover:bg-gray-100 focus:outline-none focus:ring focus:border-blue-300 mr-8 text-[#4B4B4B]">
+        <button
+          className="flex items-center px-4 py-2 border border-[#E1E1E1] rounded hover:bg-gray-100 focus:outline-none focus:ring focus:border-blue-300 mr-8 text-[#4B4B4B]"
+          onClick={() => setStep(1)}
+        >
           <IoIosArrowBack className="mr-2" /> Back
         </button>
         <button
           className="px-4 py-2 border border-[#E1E1E1
 ] rounded hover:bg-gray-100 focus:outline-none focus:ring focus:border-blue-300 w-[160px] text-[#4B4B4B]"
+          onClick={() => setStep(3)}
         >
           Skip
         </button>
