572a2d0b7419310131b52ed710f400588d7cc45f
Sing <16409791+singyiu@users.noreply.github.com>
2023-08-12 22:05:27 -0700

    MusicPulseERC6551Account

---
+++ contracts/src/MusicPulseERC6551Account.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "erc6551/src/examples/simple/SimpleERC6551Account.sol";
+
+contract MusicPulseERC6551Account is SimpleERC6551Account {}
